14a69bcac484587a6af5320a95ea25bcec5cfb8b
Tarak-Tarun <tarakcs16-G5@coastalseven.com>
2026-04-03 12:35:51 +0530

    FizzBuzz Added

---
+++ APR3/fizzbuzz.py
@@ -0,0 +1,11 @@
+def fizzbuzz(number):
+    for i in range(1,number+1):
+        if i%3==0 and i%5==0:
+            print(f"{i}: FizzBuzz")
+        elif i%3==0:
+            print(f"{i}: Fizz")
+        elif i%5==0:
+            print(f"{i}: Buzz")
+        else:
+            print(i)
+fizzbuzz(100)
