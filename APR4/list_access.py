dc2991b964b34ae447cd45bf7634a046ea79cacc
Tarak-Tarun <tarakcs16-G5@coastalseven.com>
2026-04-04 11:30:59 +0530

    List Slice,sort added

---
+++ APR4/list_access.py
@@ -0,0 +1,21 @@
+lst = []
+
+lst.append(10)
+lst.append(20)
+lst.append(30)
+lst.append(40)
+print(lst)
+
+print(lst[1:3])
+
+lst[1] = 25
+print(lst)
+
+lst.remove(30)
+print(lst)
+
+lst.sort()
+print(lst)
+
+lst.reverse()
+print(lst)
