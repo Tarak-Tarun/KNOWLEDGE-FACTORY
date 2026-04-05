9a0bcf55db5016f218237661b51b889f79585731
Tarak-Tarun <tarakcs16-G5@coastalseven.com>
2026-04-05 21:45:55 +0530

    JSON Added

--- APR4/json_i.py
+++ APR4/json_i.py
@@ -1,13 +1,9 @@
 import json
-
 data = {"name": "Tarak", "age": 20}
-
 f = open("data.json", "w")
 json.dump(data, f)
 f.close()
-
 f = open("data.json", "r")
 data = json.load(f)
 f.close()
-
 print(data)
