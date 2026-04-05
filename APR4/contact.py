1e7d1fd12d94661fe355f38a684fa351a85a95fe
Tarak-Tarun <tarakcs16-G5@coastalseven.com>
2026-04-05 21:55:17 +0530

    Contacts Task Added

--- APR4/contact.py
+++ APR4/contact.py
@@ -1,18 +1,10 @@
 import json
-
-# new contact
 new_contact = {"name": input("Name: "), "phone": input("Phone: ")}
-
-# read existing data
 try:
     with open("contacts.json", "r") as f:
         data = json.load(f)
 except:
     data = []
-
-# add new contact
 data.append(new_contact)
-
-# write back to file
 with open("contacts.json", "w") as f:
     json.dump(data, f, indent=4)
