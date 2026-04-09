08aff77577e9a8b814291b115795d3aabe4095c0
Tarak-Tarun <tarakcs16-G5@coastalseven.com>
2026-04-09 17:30:41 +0530

    Fetch Data function Added

--- APR9/database.py
+++ APR9/database.py
@@ -39,10 +39,31 @@ def insert_data(name,email,password,img_url):
         print("Data inserted successfully")
     cursor.close()
     conn.close()
+def fetch_data():
+    conn=get_connection()
+    cursor=conn.cursor()
+    select_query="""
+    SELECT * FROM users;
+    """
+    cursor.execute(select_query)
+    rows=cursor.fetchall()
+    for row in rows:
+        for i in row:
+            print(i)
+    cursor.close()
+    conn.close()
+
 if __name__=="__main__":
-    name=input("Enter name:")
-    email=input("Enter email:")
-    password=input("Enter password:")
-    img_url=input("Enter image url:")
-    password_hash=generate_password_hash(password)
-    insert_data(name, email, password_hash, img_url)
+    i=int(input("Enter 1 to insert data, 2 To Fetch Data: "))
+    
+    if i==1:
+        name=input("Enter name:")
+        email=input("Enter email:")
+        password=input("Enter password:")
+        img_url=input("Enter image url:")
+        password_hash=generate_password_hash(password)
+        insert_data(name, email, password_hash, img_url)
+    elif i==2:
+        fetch_data()
+    else:
+        print("Invalid input")
