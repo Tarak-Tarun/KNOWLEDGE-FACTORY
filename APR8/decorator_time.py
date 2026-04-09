4c4ada51d2a5ee24ffa3a19886a7f17338444e8b
Tarak-Tarun <tarakcs16-G5@coastalseven.com>
2026-04-09 23:28:13 +0530

    Decorator wraps and Time added

--- APR8/decorator_time.py
+++ APR8/decorator_time.py
@@ -3,13 +3,16 @@ from functools import wraps
 def timer(func):
     @wraps(func)
     def wrapper():
+        print("Starting the function execution.")
         start=time.time()
         func()
+        print("Function execution completed.")
         end=time.time()
         print(f"Execution time: {end-start} seconds")
     return wrapper
 @timer
 def say_hello():
-    print("Hello!")
-    time.sleep(2)
+   for i in range(100):
+        print("Hello!")
+
 say_hello()
